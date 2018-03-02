9b9acc4bb4387e2c873efc9e6a064baa8061038d
Daniel Chia <daniel_1118@hotmail.com>
2018-03-02 15:02:56 -0500

    added about page content

--- client/src/AboutPage/AboutPage.js
+++ client/src/AboutPage/AboutPage.js
@@ -1,13 +1,35 @@
 import React from 'react';
 
+import { Grid, Header, List, Segment } from 'semantic-ui-react'
+
 
 class AboutPage extends React.Component {
 	render() {
 		return (
-			<div>
-				<h2>About Page</h2>
-				<div>What does this app even do?</div>
-			</div>
+			<Grid stackable>
+				<Grid.Row centered colums={4}>
+					<Header as='h1' color='teal'>
+						About Pollster
+					</Header>
+				</Grid.Row>
+
+				<Grid.Row centered columns={2}>
+					<Grid.Column>
+						<Segment>
+							<Header as='h4' color='teal'>
+								Pollster is one of Free Code Camp's Projects (Voting App)
+							</Header>
+
+							Features of the application are:
+							<List bulleted>
+								<List.Item>You can create Polls to vote on</List.Item>
+								<List.Item>Results of the Polls are aggregated and shown in a doughnut chart</List.Item>
+								<List.Item>Authenticating opens up a few other features, ie. creating Polls and adding options to Polls</List.Item>
+							</List>
+						</Segment>
+					</Grid.Column>
+				</Grid.Row>
+			</Grid>
 		);
 	}
 }
