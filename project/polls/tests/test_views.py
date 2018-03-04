8101524b01b8b3ba41c0838b62aa4026efd44171
Daniel Chia <daniel_1118@hotmail.com>
2018-03-03 22:47:50 -0500

    added post functionality for questions, with tests

--- project/polls/tests/test_views.py
+++ project/polls/tests/test_views.py
@@ -9,16 +9,16 @@ from polls.models import Question, Choice
 from polls.serializers import QuestionSerializer, ChoiceSerializer
 
 
-class QuestionsTests(APITestCase):
+class QuestionTests(APITestCase):
 
     def setUp(self):
-        user = User.objects.create(
+        self.user = User.objects.create(
             username='dan',
             email='dan@example.com',
             password='password'
         )
-        Question.objects.create(name='Test question 1', owner=user)
-        Question.objects.create(name='Test question 2', owner=user)
+        Question.objects.create(name='Test question 1', owner=self.user)
+        Question.objects.create(name='Test question 2', owner=self.user)
 
     def test_get_questions(self):
         """ Get all questions
@@ -30,3 +30,17 @@ class QuestionsTests(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, serializer.data)
+
+    def test_create_question_valid_data(self):
+        url = reverse('questions')
+        data = {'name': 'What is love', 'owner': self.user.pk}
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_question_invalid_data(self):
+        url = reverse('questions')
+        data = {'name': '', 'owner': self.user.pk}
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
--- project/polls/views.py
+++ project/polls/views.py
@@ -5,6 +5,7 @@ from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
 
 
 # class QuestionViewSet(viewsets.ModelViewSet):
@@ -18,6 +19,13 @@ class QuestionList(APIView):
         questions = Question.objects.all()
         serializer = QuestionSerializer(questions, many=True)
         return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = QuestionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET'])
