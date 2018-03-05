8aa81497a907f82877ef5a0c28edfd883278209e
Daniel Chia <daniel_1118@hotmail.com>
2018-03-04 23:29:27 -0500

    fixed up broken user routes

--- project/accounts/serializers.py
+++ project/accounts/serializers.py
@@ -1,18 +1,16 @@
 
 from django.contrib.auth.models import User
-from rest_framework import serializers, viewsets, routers
+from rest_framework import serializers
 from polls.models import Question, Choice
 
 
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     questions = serializers.PrimaryKeyRelatedField(many=True, queryset=Question.objects.all())
 
     class Meta:
         model = User
         fields = (
             'id',
-            'url',
             'username',
             'email',
             'is_staff',
--- project/project/urls.py
+++ project/project/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import url, include
-from accounts import views as AccountsViews
-from polls import views as PollsViews
 
 
 urlpatterns = [
     url(r'^', include('polls.urls')),
+    url(r'^', include('accounts.urls')),
     url(r'^auth/', include('djoser.urls')),
     url(r'^auth/', include('djoser.urls.jwt')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
