91322406138c3af3c3d8a4f51f262395e720c7c2
Daniel Chia <daniel_1118@hotmail.com>
2018-03-16 00:41:39 -0400

    added api prefix to rest api routes

--- project/project/urls.py
+++ project/project/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import url, include
 
 
 urlpatterns = [
-    url(r'^', include('polls.urls')),
-    url(r'^', include('accounts.urls')),
-    url(r'^auth/', include('djoser.urls')),
-    url(r'^auth/', include('djoser.urls.jwt')),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/', include('polls.urls')),
+    url(r'^api/', include('accounts.urls')),
+    url(r'^api/auth/', include('djoser.urls')),
+    url(r'^api/auth/', include('djoser.urls.jwt')),
+    url(r'^api/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
