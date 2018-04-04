77ef1b8ff28f58944b98fb0cf22c8fdbdd14e237
Daniel Chia <danielarmandochia@gmail.com>
2018-04-04 11:18:00 -0400

    switched out db to sqlite, fix up requirements

--- project/project/settings.py
+++ project/project/settings.py
@@ -78,13 +78,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'pollster',
-        'USER': 'daniel',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '',
+    'default':
+    {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
